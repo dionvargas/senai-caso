730857a095a90d00577a74a523bc4edd521ac911
Dionathan Vargas <dionathan_vargas@hotmail.com>
2024-09-19 11:20:24 -0300

    Atualizada etapa de análise

---
+++ README.md
@@ -0,0 +1,74 @@
+# 🚀 Projeto de Classificação de Dados com Inteligência Artificial
+
+<div style="display: inline_block">
+  <img src="https://img.shields.io/badge/Python-3.12.4-blue">
+</div>
+
+## 📄 Descrição
+
+Este projeto utiliza técnicas de inteligência artificial para classificar dados provenientes de 5 sensores diferentes. Cada sensor coleta 200 sinais com uma taxa de amostragem de 10kHz. O objetivo é treinar um modelo de machine learning para classificar esses sinais em 5 classes distintas.
+
+## 📚 Índice
+
+- Instalação
+- Uso
+- Estrutura do Projeto
+- Funcionalidades
+- Contribuição
+- Licença
+- Contato
+
+## 🛠️ Instalação
+
+Siga os passos abaixo para configurar o ambiente e instalar as dependências necessárias:
+
+```bash
+# Clone o repositório
+git clone https://github.com/usuario/senai-caso.git
+
+# Entre no diretório do projeto
+cd senai-caso
+
+# Crie um ambiente virtual
+python -m venv venv
+
+# Ative o ambiente virtual
+# No Windows
+venv\Scripts\activate
+# No macOS/Linux
+source venv/bin/activate
+
+# Instale as dependências
+pip install -r requirements.txt
+```
+
+## 🚀 Uso
+Exemplos de como usar o projeto para treinar e avaliar o modelo de classificação:
+
+``` bash
+# Execute o arquivo app.py com python
+python app.py
+```
+Abra um navegador no diretório: [http://127.0.0.1:5000/](http://127.0.0.1:5000/)
+
+## ✨ Funcionalidades
+- Classificação de sinais de sensores em 5 classes distintas.
+- Treinamento e avaliação de modelos de machine learning.
+- Análise exploratória dos dados.
+
+## 🤝 Contribuição
+Se você deseja contribuir, siga as diretrizes abaixo:
+
+1. Faça um fork do projeto
+2. Crie uma branch para sua feature (git checkout -b feature/nova-feature)
+3. Commit suas mudanças (git commit -m 'Adiciona nova feature')
+4. Faça um push para a branch (git push origin feature/nova-feature)
+5. Abra um Pull Request
+
+## 📜 Licença
+Este projeto está licenciado sob a Licença MIT - veja o arquivo LICENSE para mais detalhes.
+
+## 📞 Contato
+Dionathan Luan de Vargas - @dionvargas - dionathan_vargas@hotmail.com
+
+Link do Projeto: https://github.com/dionvargas/caso-senai
--- app.py
+++ app.py
@@ -14,18 +14,54 @@ def index():
 
 @app.route('/analise', methods=['GET', 'POST'])
 def analise():
-    df_data1 = pd.DataFrame(substituir_NaN(np.load('./dados/Dados_1.npy')))
-    df_data2 = pd.DataFrame(substituir_NaN(np.load('./dados/Dados_2.npy')))
-    df_data3 = pd.DataFrame(substituir_NaN(np.load('./dados/Dados_3.npy')))
-    df_data4 = pd.DataFrame(substituir_NaN(np.load('./dados/Dados_4.npy')))
-    df_data5 = pd.DataFrame(substituir_NaN(np.load('./dados/Dados_5.npy')))
-    
+    # Lê os dados
+    classes = np.load('./dados/Classes.npy', allow_pickle=True)
+    df_data1 = pd.DataFrame(np.load('./dados/Dados_1.npy'))
+    df_data2 = pd.DataFrame(np.load('./dados/Dados_2.npy'))
+    df_data3 = pd.DataFrame(np.load('./dados/Dados_3.npy'))
+    df_data4 = pd.DataFrame(np.load('./dados/Dados_4.npy'))
+    df_data5 = pd.DataFrame(np.load('./dados/Dados_5.npy'))
+
+    data_classes = {}
+    valores_unicos, contagens = np.unique(classes, return_counts=True)
+    for i in range(len(valores_unicos)):
+        data_classes[valores_unicos[i]] = contagens[i]
+    print(valores_unicos, contagens)
+    print (data_classes)
+
+    def resume_sensor(df_sensor):
+        resumo = {
+            "canais": df_sensor.columns.tolist(),
+            "formato": df_sensor.shape,
+            "minimo": df_sensor.min(axis=1).min(),
+            "maximo": df_sensor.max(axis=1).max(),
+            "inf": np.isinf(df_sensor.values).any(),
+            "NaN": df_sensor.isnull().values.any()
+        }
+        return resumo
+
+    # Resumo dos dados
+    data_sensores = {
+        "sensor1": resume_sensor(df_data1),
+        "sensor2": resume_sensor(df_data2),
+        "sensor3": resume_sensor(df_data3),
+        "sensor4": resume_sensor(df_data4),
+        "sensor5": resume_sensor(df_data5)
+    }
+
+    # Substitui os NaN para zero para poderem ser plotados
+    df_data1 = df_data1.fillna(0)
+    df_data2 = df_data2.fillna(0)
+    df_data3 = df_data3.fillna(0)
+    df_data4 = df_data4.fillna(0)
+    df_data5 = df_data5.fillna(0)
+
     if request.method == 'POST':
-        selected_sensor1 = request.form.get('sensor1')
-        selected_sensor2 = request.form.get('sensor2')
-        selected_sensor3 = request.form.get('sensor3')
-        selected_sensor4 = request.form.get('sensor4')
-        selected_sensor5 = request.form.get('sensor5')
+        selected_sensor1 = request.form.get('canal_sensor1')
+        selected_sensor2 = request.form.get('canal_sensor2')
+        selected_sensor3 = request.form.get('canal_sensor3')
+        selected_sensor4 = request.form.get('canal_sensor4')
+        selected_sensor5 = request.form.get('canal_sensor5')
     else: 
         selected_sensor1 = '0'
         selected_sensor2 = '0'
@@ -33,28 +69,22 @@ def analise():
         selected_sensor4 = '0'
         selected_sensor5 = '0'
 
-    df = pd.DataFrame()
+    df_graph = pd.DataFrame()
+    df_graph['S1-C' + selected_sensor1] = df_data1[int(selected_sensor1)]
+    df_graph['S2-C' + selected_sensor2] = df_data2[int(selected_sensor2)]
+    df_graph['S3-C' + selected_sensor3] = df_data3[int(selected_sensor3)]
+    df_graph['S4-C' + selected_sensor4] = df_data4[int(selected_sensor4)]
+    df_graph['S5-C' + selected_sensor5] = df_data5[int(selected_sensor5)]
 
-    if(selected_sensor1 != "Não mostrar"):
-        df['S1-C' + selected_sensor1] = df_data1[int(selected_sensor1)]
-    if(selected_sensor2 != "Não mostrar"):
-        df['S2-C' + selected_sensor2] = df_data2[int(selected_sensor2)]
-    if(selected_sensor2 != "Não mostrar"):
-        df['S3-C' + selected_sensor3] = df_data3[int(selected_sensor3)]
-    if(selected_sensor2 != "Não mostrar"):
-        df['S4-C' + selected_sensor4] = df_data4[int(selected_sensor4)]
-    if(selected_sensor2 != "Não mostrar"):
-        df['S5-C' + selected_sensor5] = df_data5[int(selected_sensor5)]
+    fig = px.line(df_graph, x=range(0,df_graph.shape[0]), y=df_graph.columns)
+    fig.update_layout(
+        xaxis_title="t(s)",
+        yaxis_title="Valor",
+        legend_title="Canais"
+    )
+    graph_html = pio.to_html(fig, full_html=False, config={'displaylogo': False})
 
-    fig = px.line(df, x=range(0,df.shape[0]), y=df.columns)
-    graph_html = pio.to_html(fig, full_html=False)
-
-    return render_template('analise.html', graph_html=graph_html,
-                           data_sensor_1=df_data1.columns,
-                           data_sensor_2=df_data2.columns,
-                           data_sensor_3=df_data3.columns,
-                           data_sensor_4=df_data4.columns,
-                           data_sensor_5=df_data5.columns)
+    return render_template('analise.html', graph_html=graph_html, data = data_sensores, data_classes = data_classes)
 
 @app.route('/tratamento', methods=['GET', 'POST'])
 def tratamento():
